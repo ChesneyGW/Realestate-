ad0061a8fca0701f00ff61a10940f4affd7ac224
Chesney Williams <chesneygwilliams@gmail.com>
2021-06-12 23:30:08 -0400

    1st push

---
+++ src/data/InfoData.js
@@ -0,0 +1,25 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/house-5.jpg';
+
+
+export const InfoData = {
+    heading: 'Explore our beautiful home',
+    paragraphOne: 'Lorem ipsum dolor, sit amet.',
+    paragraphTwo: 'Esse quaerat vel at officis, sit cumque!',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 
+    'Lorem ipsum dolor, sit amet.',
+    paragraphTwo: 
+    'Esse quaerat vel at officis, sit cumque!',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: false,
+    delay: 300
+};
